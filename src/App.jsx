5ac7fdb39386446ce7cec00ffca46f910d88a422
ViniVanC <vincentvan0302@gmail.com>
2023-03-16 19:01:54 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Watch } from "./components/Watch";
+
 function App() {
-  return <div className="flex h-[100vh] items-center justify-center">app</div>;
+  return (
+    <div className="flex h-[100vh] flex-col items-center justify-center bg-[#ecf0f3]">
+      <Watch />
+    </div>
+  );
 }
 
 export default App;
